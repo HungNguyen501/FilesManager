38e3fc7da6a3dd4f00bc41be422bc3ad568bf544
Nguyen Dinh Hung <chelseazodoi@gmail.com>
2020-06-16 19:32:46 +0700

    first commit

---
+++ app/src/main/java/com/example/filesmanagement/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.example.filesmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class Main2Activity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        listView = findViewById(R.id.list_view_2);
+    }
+}
